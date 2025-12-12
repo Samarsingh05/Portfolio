c58159adc52d3caa10f4c5c8653efa2e2cf1951d
Samar Singh <90522267+Samarsingh05@users.noreply.github.com>
2025-12-13 00:38:48 +0530

    Change base path from '/My-Portfolio/' to '/My_Portfolio/'

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/My-Portfolio/",
+  base: "/My_Portfolio/",
   server: {
     host: "::",
     port: 8080,
